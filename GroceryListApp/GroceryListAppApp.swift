332112e18d28f100bc6092d980f39e764d2085de
Jonathan Engelsma <jengelsma@gmail.com>
2025-03-28 14:38:07 -0400

    Initial Commit

---
+++ GroceryListApp/GroceryListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListAppApp.swift
+//  GroceryListApp
+//
+//  Created by Jonathan Engelsma on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
