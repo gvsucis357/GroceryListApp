86f3793d9af30007015928b52cd158337e20bff6
Jonathan Engelsma <jengelsma@gmail.com>
2025-03-31 17:28:11 -0400

    Firebase SDK dependency added with startup code.

--- GroceryListApp/GroceryListAppApp.swift
+++ GroceryListApp/GroceryListAppApp.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("ApplicationDelegate didFinishLaunchingWithOptions.")
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+
 @main
 struct GroceryListAppApp: App {
     @StateObject var groceryViewModel = GroceryViewModel()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
